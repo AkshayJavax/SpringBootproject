25a6bbe428c52d6169df0c07a4475d44f40fbd0f
AkshayJavax <akshaykokane019@gmail.com>
2025-09-27 17:30:51 +0530

    Updated file added

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,11 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
         System.out.println("file change");
+
 		String st="Hello";
+
+        int x=10;
+
         System.out.println("Hii... Akshay Akshay");
         SpringApplication.run(DemoApplication.class, args);
 	}
