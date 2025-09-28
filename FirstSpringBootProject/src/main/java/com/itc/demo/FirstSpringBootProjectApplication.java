66e2c33162c70b05826f3d50cd5a65937ec10df1
AkshayJavax <akshaykokane019@gmail.com>
2025-09-28 14:17:11 +0530

    first commit

---
+++ FirstSpringBootProject/src/main/java/com/itc/demo/FirstSpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.itc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("FirstSpringBootProjectApplication.main()");
+		SpringApplication.run(FirstSpringBootProjectApplication.class, args);
+	}
+
+}
