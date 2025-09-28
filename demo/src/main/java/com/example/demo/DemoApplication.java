fa8111438fa455373507f8f50240a49537725b02
AkshayJavax <akshaykokane019@gmail.com>
2025-09-28 13:45:42 +0530

    2 files modified

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,12 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-        System.out.println("file change");
+        System.out.println("Hey There This is my spring boot app");
 
-		String st="Hello";
-
-        int x=10;
-System.out.println("Hii... Akshay Akshay");
         SpringApplication.run(DemoApplication.class, args);
 	}
 
